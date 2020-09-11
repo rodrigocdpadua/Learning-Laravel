570dc6f1d74511591a83fb27c9086a0772b2389b
rodrigocdpadua <60583119+rodrigocdpadua@users.noreply.github.com>
2020-09-11 14:06:19 -0300

    AprendendoRotas

---
+++ app/Http/Controllers/Rotas/RotasController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Rotas;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RotasController extends Controller
+{
+    public function testerotas(){
+        return 'Teste Rotas com Controller';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,53 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//Grupo de Rotas
+Route::prefix('produto') -> group(function(){
+    Route::get('/1', function(){
+        return 'PRODRUTO 1';
+    });
+
+    Route::get('/2', function(){
+        return 'PRODRUTO 2';
+    });
+
+    Route::get('/', function(){
+        return 'PRODUTO';
+    });
+});
+
+//Rotas Nomeadas
+Route::get('/proibido3', function(){
+    return redirect() -> route('nomeado');
+});
+
+Route::get('/name', function(){
+    return 'Hey Hey Hey';
+}) -> name('nomeado');
+
+//Redirecionamento de Rotas
+Route::redirect('/proibido', '/');
+
+Route::get('/proibido2', function(){
+    return redirect('/');
+});
+
+//Definir Rotas com Parâmetros não Obrigatórios
+Route::get('/vinhos/{vin?}', function($vinho = ''){
+    return "Vinho(s) {$vinho}";
+});
+
+//Definir Rotas com Parâmetros
+Route::get('/categoria/{cat}', function($cat){
+    return "Categoria: {$cat}";
+});
+
+//Definir Rotas
+Route::get('/teste', function () {
+    return "Testei";
+});
+
+Route::get('/testecontroller', 'Rotas\RotasController@testerotas');
 
 Route::get('/', function () {
     return view('welcome');
